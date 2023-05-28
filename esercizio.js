9e0d5f9650cf1e893338b6909c1a3ce0eb945ebc
Miriammar <132224854+Miriammar@users.noreply.github.com>
2023-05-28 23:05:30 +0200

    Add files via upload

---
+++ esercizio.js
@@ -0,0 +1,56 @@
+
+/* i datatype sono delle categorie di dati che si possono assegnare a Javascrpit. Sei sono i diversi tipi:
+
+String: sequenza di caratteri delimitati da virgolette "" oppure gli apici '' o ``
+Boolean: costruito sulla base del valore booleano (vero o falso)
+Numero: Dato prettamente numerico. Javascript non differenzia i numeri interi da quelli decimali. Ci sono anche valori speciali, tra cui l'infinity (+ e -) o il Not a number (divisione o moltiplicazione x 0)
+ Undefined: Dichiara il valore che ancora non esiste
+ Null: valore vuoto */
+
+let x = 12;
+let name = 'Miriam'
+
+
+
+let sottrazione = x - 4
+
+console.log('RISULTATO SOTTRAZIONE:', sottrazione)
+
+
+const str2 = `Risultato della sottrazione:${sottrazione}`
+
+
+console.log('STRINGA:', str2)
+
+
+let io = {
+    name : 'Miriam',
+    surname: 'marinelli'
+}
+
+
+const array = [
+    io
+]
+
+console.log (array)
+
+
+let name1 = 'john'
+let name2 = 'John'
+
+var Boolean = name1 === 'name2'
+console.log(Boolean)
+
+public class JohnUpperLowerCase {
+    public static void main (Strings [] args){
+    namename1, name2
+
+    name1 = 'John';
+    name2 = 'john'
+
+    'name' name2UpperCase = name. toUpperCase (name1)
+    'name' name1LowerCae = name. toLowerCase (name2)
+
+}
+}
